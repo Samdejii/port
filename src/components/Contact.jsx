a51b8407fa29b867a9a7e3fda5bd8d240a4aebea
Samuel Adewumi <samdejii97@gmail.com>
2023-11-28 10:18:07 +0100

    second commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#131316] flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/7c814c3d-b884-4736-8f17-abaf64cfd577" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/6281f523-ef8e-4bfe-b4ed-5336c77b7650" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-yellow-400 text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - adewumisam07@gmail.com</p>
